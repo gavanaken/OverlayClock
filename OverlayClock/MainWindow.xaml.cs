16f24f696d7dcdc31776d86796c00952d6963a1c
Greg Van Aken <gvanaken@gmail.com>
2023-02-24 17:28:45 -0500

    Initial commit

---
+++ OverlayClock/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OverlayClock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += new RoutedEventHandler(Window_Loaded);
+        }
+
+        void myDispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            lblClock.Content = DateTime.Now.ToString("hh:mm");
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+            this.Left = desktopWorkingArea.Left;
+            this.Top = desktopWorkingArea.Bottom - this.Height;
+
+            System.Windows.Threading.DispatcherTimer myDispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            myDispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 100); // 100 Milliseconds  
+            myDispatcherTimer.Tick += myDispatcherTimer_Tick;
+            myDispatcherTimer.Start();
+        }
+    }
+}
